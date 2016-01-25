2d7680ea3cac4f5fca611b9efaaa7d6d551a1e9f
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-25 05:36:47 -0500

    minor formatting

--- index.js
+++ index.js
@@ -9,13 +9,27 @@
 'use strict';
 
 module.exports = function(num) {
-  var lookup = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
   var numeral = '';
+  var lookup = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
 
-  for (var i in lookup) {
-    while (num >= lookup[i]) {
-      numeral += i;
-      num -= lookup[i];
+  for (var key in lookup) {
+    while (num >= lookup[key]) {
+      numeral += key;
+      num -= lookup[key];
     }
   }
   return numeral;
