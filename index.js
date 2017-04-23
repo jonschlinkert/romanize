5f8dd9fd40515ae886a9f5cb8e49c9680ab2c7c1
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-23 01:45:51 -0400

    use repeat-string

--- index.js
+++ index.js
@@ -8,17 +8,17 @@
 
 'use strict';
 
+var repeat = require('repeat-string');
 var numerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
 
 module.exports = function(num) {
   var keys = Object.keys(numerals);
-  var numeral = '';
+  var str = '';
+
   for (var i = 0; i < keys.length; i++) {
     var key = keys[i];
-    while (num >= numerals[key]) {
-      numeral += key;
-      num -= numerals[key];
-    }
+    str += repeat(key, num / numerals[key] >>> 0);
+    num %= numerals[key];
   }
-  return numeral;
+  return str;
 };
